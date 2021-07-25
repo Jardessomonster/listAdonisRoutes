1dad108c64b1a6bebeac7069097ebdf504fa060c
unknown <jrjardeson@gmail.com>
2021-07-25 14:49:53 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import re
+import openpyxl
+
+
+modules = {
+    'social-network': '../incicle/social-media/back-social-media',
+    'core': '../incicle/core/core-backend'
+}
+
+print(modules['social-network'])
